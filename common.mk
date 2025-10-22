f5ee73646f2bc53951ab32780d94832b06b8d6b3
Leviscape <leviscape.xyz@gmail.com>
2025-10-22 16:19:21 +0530

    sm8550-common: Move BOARD_SHIPPING_API_LEVEL to common makefile
    Change-Id: I98ebec6b0a7d5e1fe0408561ead37174cd48ee02

--- common.mk
+++ common.mk
@@ -319,6 +319,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_kalama/android.hardware.sensor.stepdetector.xml
 
 # Shipping API level
+BOARD_SHIPPING_API_LEVEL := 33
 PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
