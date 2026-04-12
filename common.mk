14a1ed2b29ec92d211db3fc586e1a3ea317860b9
zingrx <mnitpro@gmail.com>
2026-04-12 16:01:50 +0300

    common: Add high touch polling path configuration for Xiaomi touch service

--- common.mk
+++ common.mk
@@ -366,6 +366,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.xiaomi
 
+$(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/devices/virtual/touch/touch_dev/bump_sample_rate)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
